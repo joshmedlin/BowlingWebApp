8d51fc294bd9e4c9ffaadf60f2c60861129f6958
Doug Stieby <doug.stieby@scifusion360.com>
2018-02-08 09:06:57 -0500

    ability to create new members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,29 @@
 class MembersController < ApplicationController
-end
+
+
+    def create
+
+      @member = Member.new(member_params)
+      @member.save
+      redirect_to members_path
+    end
+
+    def show
+      @member = Member.find(params[:id])
+    end
+
+    def index
+      @members = Member.all
+    end
+
+    def destroy
+      @member = Member.find(params[:id])
+      @member.destroy
+      redirect_to members_path
+    end
+
+    private
+    def member_params
+      params.require(:member).permit(:name)
+    end
+  end
