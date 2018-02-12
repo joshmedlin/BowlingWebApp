d5268bd5d53da9c070b43cd9a4b2086bff62509f
Doug Stieby <doug.stieby@scifusion360.com>
2018-02-12 16:35:42 -0500

    create TeamInDay when new team is created

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
+  get 'members/new'
+  get 'members/index'
+  get 'teams/index'
+  get 'teams/new'
+  get 'team_destroy' => 'teams#destroy'
 
   resources :members
   resources :scores
   resources :rounds
   resources :teams
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'members/new'
-  get 'members/index'
-  get 'teams/index'
-  get 'teams/new'
+
 
   root 'rounds#index'
 end
