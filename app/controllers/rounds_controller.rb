2492e041349dba9384da6eff865f670ee0f94960
joshmedlin <josh.medlin@scifusion360.com>
2018-02-12 14:16:36 -0500

    Fixing some changes from merge and crate day when you visit site on a new day

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -11,14 +11,26 @@ class RoundsController < ApplicationController
   def update
     p params
     round_id = Round.last.id
+    worked = true
       params[:member_id].each do |value|
         @score = Score.new
         @score.member_id = MembersInTeam.find(value).member_id
         @score.round_id = round_id
         @score.score = params[value]
-        @score.save
         p @score
+        if @score.save
+
+        else
+          worked = false
         end
+      end
+    if worked
+      render 'rounds/index'
+    else
+      render 'rounds/index'
+    end
+
+
   end
 
   def index
