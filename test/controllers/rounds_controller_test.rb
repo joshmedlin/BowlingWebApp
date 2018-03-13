57d16a2c4da88ed3e90fd64807d156b14f95c5ed
Doug Stieby <doug.stieby@scifusion360.com>
2018-03-13 16:26:14 -0400

    bundle install --path vendor/bundle

--- test/controllers/rounds_controller_test.rb
+++ test/controllers/rounds_controller_test.rb
@@ -1,14 +1,5 @@
 require 'test_helper'
 
 class RoundsControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get rounds_show_url
-    assert_response :success
-  end
-
-  test "should get index" do
-    get rounds_index_url
-    assert_response :success
-  end
 
 end
