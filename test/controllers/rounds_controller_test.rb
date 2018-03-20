2c79e5ae54db28fd3615a32a6e978d82e8c93515
Doug Stieby <doug.stieby@scifusion360.com>
2018-03-20 13:01:48 -0400

    Revert "bundle install --path vendor/bundle"
    This reverts commit 57d16a2c4da88ed3e90fd64807d156b14f95c5ed.

--- test/controllers/rounds_controller_test.rb
+++ test/controllers/rounds_controller_test.rb
@@ -1,5 +1,14 @@
 require 'test_helper'
 
 class RoundsControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get rounds_show_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get rounds_index_url
+    assert_response :success
+  end
 
 end
